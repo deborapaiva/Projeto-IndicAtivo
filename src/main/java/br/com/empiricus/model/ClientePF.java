ade2f219e03ad57ba684a4976ebdfa446280df4d
deborapaiva <debora.rubim.paiva@outlook.com>
2022-11-20 01:31:39 -0300

    Atualização 15

--- src/main/java/br/com/empiricus/model/ClientePF.java
+++ src/main/java/br/com/empiricus/model/ClientePF.java
@@ -47,8 +47,6 @@ public class ClientePF {
 	@Column(name = "telefone")
 	private String telefone;
 	
-	private String token;
-
 	public Long getId() {
 		return id;
 	}
@@ -103,13 +101,5 @@ public class ClientePF {
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
-	}
-	
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
 	}
 }
