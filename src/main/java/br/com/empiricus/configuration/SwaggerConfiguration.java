ee9c238865d8560cc9c1c7abcb8ddd1787180e1d
deborapaiva <debora.rubim.paiva@outlook.com>
2022-11-19 21:25:48 -0300

    atualização 2

--- src/main/java/br/com/empiricus/configuration/SwaggerConfiguration.java
+++ src/main/java/br/com/empiricus/configuration/SwaggerConfiguration.java
@@ -1,5 +1,45 @@
 package br.com.empiricus.configuration;
 
-public class SwaggerConfiguration {
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "Utilize o token para acessar o IndicAtivo"
+)
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(
+                new Info()
+                        .title("IndicAtivo API RESTful")
+                        .version("v1")
+                        .description("API RESTful do IndicAtivo")
+                        .termsOfService("link do repositorio do projeto")
+                        .license(
+                                new License()
+                                        .name("Apache License 2.0")
+                                        .url("https://www.apache.org/licenses/LICENSE-2.0")
+                        )
+                        .contact(
+                                new Contact()
+                                        .name("IndicAtivo")
+                                        .email("email")
+                                        .url("swagger-ui do projeto")
+                        )
+                        .summary(
+                                "Descrição"
+                        )
+        );
+    }
 }
